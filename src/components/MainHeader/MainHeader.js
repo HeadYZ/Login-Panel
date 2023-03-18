254255b7d6d2259bec5e2fd71881f5cdceef78a4
Patryk <krasowski.p@outlook.com>
2023-03-18 22:04:03 +0100

    added AuthContent and authorizes navigation

--- src/components/MainHeader/MainHeader.js
+++ src/components/MainHeader/MainHeader.js
@@ -1,11 +1,14 @@
-import classes from './MainHeader.module.css'
+import { useContext } from 'react'
 import Navigation from './Navigation'
+import classes from './MainHeader.module.css'
+import AuthContext from '../store/auth-context'
 
 const MainHeader = () => {
+	const ctx = useContext(AuthContext)
 	return (
 		<header className={classes.header}>
 			<p>A Typical Page</p>
-			<Navigation />
+			{ctx.isLoggedIn && <Navigation />}
 		</header>
 	)
 }
