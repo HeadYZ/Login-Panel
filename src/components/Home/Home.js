4fb92b1727b8c7c4dbbfd9d6a397d52176ffe55e
Patryk <krasowski.p@outlook.com>
2023-03-18 22:05:14 +0100

    added AuthContext

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,11 +1,16 @@
 import Button from '../UI/Button'
+import { useContext } from 'react'
 import classes from './Home.module.css'
+import AuthContext from '../store/auth-context'
 
-const Home = () => {
-	return <div className={classes.home}>
-        <h1>Welcome back !</h1>
-        <Button>Logout</Button>
-    </div>
+const Home = props => {
+	const ctx = useContext(AuthContext)
+	return (
+		<div className={classes.home}>
+			<h1>Welcome back !</h1>
+			<Button onClick={ctx.onLogout}>Logout</Button>
+		</div>
+	)
 }
 
 export default Home
