181780349c6ee17b458371208788ca883a64b574
Patryk <krasowski.p@outlook.com>
2023-03-18 22:04:45 +0100

    added AuthContext

--- src/components/MainHeader/Navigation.js
+++ src/components/MainHeader/Navigation.js
@@ -1,13 +1,19 @@
+import { useContext } from 'react'
+import AuthContext from '../store/auth-context'
 import classes from './Navigation.module.css'
 
-const Navigation = () => {
+const Navigation = props => {
+	const ctx = useContext(AuthContext)
+
 	return (
 		<nav>
 			<ul className={classes.nav__panel}>
 				<li className={classes['nav__panel-item']}>Users</li>
 				<li className={classes['nav__panel-item']}>Admin</li>
 				<li className={classes['nav__panel-item']}>
-					<button className={classes.nav__button}>Logout</button>
+					<button className={classes.nav__button} onClick={ctx.onLogout}>
+						Logout
+					</button>
 				</li>
 			</ul>
 		</nav>
