b2f6148670ab4c2299cb2c6b20bb8fa5e3033aac
Patryk <krasowski.p@outlook.com>
2023-03-18 22:00:47 +0100

    Added loginHandler, logutHandler, Provider

--- src/App.js
+++ src/App.js
@@ -1,24 +1,43 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import Button from './components/UI/Button'
 import classes from './App.module.css'
 import Navigation from './components/MainHeader/Navigation'
 import Login from './components/Login/Login'
 import Home from './components/Home/Home'
 import MainHeader from './components/MainHeader/MainHeader'
+import AuthContext from './components/store/auth-context'
 
 function App() {
 	const [isLoggedIn, setIsLoggedIn] = useState(false)
 
 	const loginHandler = () => {
-
+		setIsLoggedIn(true)
+		localStorage.setItem('isLoggedIn', '1')
 	}
 
+	const logoutHandler = () => {
+		setIsLoggedIn(false)
+		localStorage.setItem('isLoggedIn', '0')
+	}
+
+	useEffect(() => {
+		const storageLoginInfo = localStorage.getItem('isLoggedIn')
+		if (storageLoginInfo === '1') {
+			setIsLoggedIn(true)
+		}
+	}, [])
+
 	return (
-		<React.Fragment>
+		<AuthContext.Provider
+			value={{
+				isLoggedIn: isLoggedIn,
+				onLogout: logoutHandler,
+				onLogin: loginHandler
+			}}>
 			<MainHeader></MainHeader>
 			{isLoggedIn && <Home></Home>}
 			{!isLoggedIn && <Login></Login>}
-		</React.Fragment>
+		</AuthContext.Provider>
 	)
 }
 
